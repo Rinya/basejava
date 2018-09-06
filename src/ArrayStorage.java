e56b4722d084c93b2611ee6bd6ef588cdaaea2ae
rinat1982@inbox.ru <va2se4k6>
2018-09-06 23:01:34 +0300

    Правки согласно замечаниям

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -9,12 +9,12 @@ class ArrayStorage {
     private int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     void save(Resume resume) {
-        if (existsInStorage(resume.uuid) != null) {
+        if (getIndexOf(resume.uuid) != null) {
             System.out.println("Resume " + resume + " already exists in storage");
         } else if (STORAGE_SIZE == size) {
             System.out.println("Storage is already full");
@@ -25,7 +25,7 @@ class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        Integer index = existsInStorage(uuid);
+        Integer index = getIndexOf(uuid);
         if (index != null) {
             return storage[index];
         } else {
@@ -36,9 +36,12 @@ class ArrayStorage {
     }
 
     void delete(String uuid) {
-        Integer index = existsInStorage(uuid);
+        Integer index = getIndexOf(uuid);
         if (index != null) {
-            System.arraycopy(storage, index + 1, storage, index, (storage.length - index - 1));
+            //Т.к. простой массив, делаем простой заменой элемента
+            //System.arraycopy(storage, index + 1, storage, index, (STORAGE_SIZE - index - 1));
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
             size--;
         } else {
             System.out.println("Resume does not exist in storage");
@@ -53,15 +56,11 @@ class ArrayStorage {
     }
 
     void update(Resume resume) {
-        if (resume != null) {
-            Integer index = existsInStorage(resume.uuid);
-            if (index != null) {
-                storage[index] = resume;
-            } else {
-                System.out.println("Resume " + resume + " does not exists in storage");
-            }
+        Integer index = getIndexOf(resume.uuid);
+        if (index != null) {
+            storage[index] = resume;
         } else {
-            System.out.println("Resume " + resume + " does not exists");
+            System.out.println("Resume " + resume + " does not exists in storage");
         }
     }
 
@@ -69,10 +68,9 @@ class ArrayStorage {
         return size;
     }
 
-    private Integer existsInStorage(String uuid) {
+    private Integer getIndexOf(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
-                //return storage[i];
                 return i;
             }
         }
