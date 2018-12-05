ba4bba70116d83bfae07c26dfb03d85b049bc644
rinat1982@inbox.ru <va2se4k6>
2018-12-06 00:14:34 +0300

    7 lesson, removed check for null in comparator

--- src/java/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/java/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -6,17 +6,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-    static final Comparator<Resume> RESUME_COMPARATOR = (first, second) -> {
-        if (first == null && second == null) {
-            return 0;
-        } else if (first == null) {
-            return -1;
-        } else  if (second == null) {
-            return 1;
-        }
-
-        return first.getUuid().compareTo(second.getUuid());
-    };
+    static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
 
     @Override
     protected Integer getSearchKey(String condition) {
