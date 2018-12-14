9a65c79e7790bb85cf31071da65d86fa47879917
rinat1982@inbox.ru <va2se4k6>
2018-12-15 01:02:01 +0300

    8 lesson, complete home work

--- src/java/ru/javawebinar/basejava/model/Resume.java
+++ src/java/ru/javawebinar/basejava/model/Resume.java
@@ -9,11 +9,14 @@ import java.util.UUID;
  * Initial resume class
  */
 public class Resume implements Comparable<Resume> {
+
     // Unique identifier
     private final String uuid;
+
     private final String fullName;
-    private Map<ContactType, String> contacts;
-    private Map<SectionType, Section> sections;
+
+    private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
+    private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
@@ -22,63 +25,57 @@ public class Resume implements Comparable<Resume> {
     public Resume(String uuid, String fullName) {
         Objects.requireNonNull(uuid, "uuid must not be null");
         Objects.requireNonNull(fullName, "fullName must not be null");
-
         this.uuid = uuid;
         this.fullName = fullName;
-        this.contacts = new EnumMap<>(ContactType.class);
-        this.sections = new EnumMap<>(SectionType.class);
     }
 
     public String getUuid() {
         return uuid;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getContact(ContactType type) {
+        return contacts.get(type);
     }
 
-    public Map<ContactType, String> getContacts() {
-        return contacts;
+    public Section getSection(SectionType type) {
+        return sections.get(type);
     }
 
-    public Map<SectionType, Section> getSections() {
-        return sections;
+    public void addContact(ContactType type, String value) {
+        contacts.put(type, value);
+    }
+
+    public void addSection(SectionType type, Section section) {
+        sections.put(type, section);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Resume)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
 
         Resume resume = (Resume) o;
 
-        if (!getUuid().equals(resume.getUuid())) return false;
-        if (!getFullName().equals(resume.getFullName())) return false;
-        if (getContacts() != null ? !getContacts().equals(resume.getContacts()) : resume.getContacts() != null)
-            return false;
-        return getSections() != null ? getSections().equals(resume.getSections()) : resume.getSections() == null;
+        if (!uuid.equals(resume.uuid)) return false;
+        return fullName.equals(resume.fullName);
+
     }
 
     @Override
     public int hashCode() {
-        int result = getUuid().hashCode();
-        result = 31 * result + getFullName().hashCode();
-        result = 31 * result + (getContacts() != null ? getContacts().hashCode() : 0);
-        result = 31 * result + (getSections() != null ? getSections().hashCode() : 0);
+        int result = uuid.hashCode();
+        result = 31 * result + fullName.hashCode();
         return result;
     }
 
     @Override
     public String toString() {
-        return uuid + ", " +
-                fullName + ", " +
-                contacts + ", " +
-                sections;
+        return uuid + '(' + fullName + ')';
     }
 
     @Override
-    public int compareTo(Resume resume) {
-        int fullNameCompare = fullName.compareTo(resume.getFullName());
-        return fullNameCompare != 0 ? fullNameCompare : uuid.compareTo(resume.uuid);
+    public int compareTo(Resume o) {
+        int cmp = fullName.compareTo(o.fullName);
+        return cmp != 0 ? cmp : uuid.compareTo(o.uuid);
     }
 }
